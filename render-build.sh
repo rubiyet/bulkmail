f486241e4302d69adbb549e7054359419a6242ba
Rubiyet Fardous <57152712+rubiyet@users.noreply.github.com>
2024-11-16 04:42:41 +0600

    build issue fix

--- render-build.sh
+++ render-build.sh
@@ -5,6 +5,10 @@ set -o errexit
 # Install all dependencies
 npm install
 
+# Define cache directories
+PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
+XDG_CACHE_HOME=/opt/render/.cache
+
 # Store or pull the Puppeteer cache from the build cache
 if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
   echo "...Copying Puppeteer Cache from Build Cache"
