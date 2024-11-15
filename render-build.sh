ca3357aa8c2c9730b9df99076c242436749de798
Rubiyet Fardous <57152712+rubiyet@users.noreply.github.com>
2024-11-16 04:45:30 +0600

    build issue fix

--- render-build.sh
+++ render-build.sh
@@ -9,11 +9,12 @@ npm install
 PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
 XDG_CACHE_HOME=/opt/render/.cache
 
-# Store or pull the Puppeteer cache from the build cache
-if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
+# Check if the Puppeteer cache directory exists
+if [[ -d $XDG_CACHE_HOME/puppeteer ]]; then
   echo "...Copying Puppeteer Cache from Build Cache"
   cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
-else 
-  echo "...Storing Puppeteer Cache in Build Cache"
-  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
+else
+  echo "...Puppeteer cache directory does not exist, skipping copy."
 fi
+
+# Continue with the build
